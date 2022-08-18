1a8f90b250a138832aa882b58c4d220f21ca029e
zvsx001 <zvsx001@gmail.com>
2022-08-18 14:00:46 +0300

    remove popular words, because it creates conflicts

--- scripts/git/check-comments.sh
+++ scripts/git/check-comments.sh
@@ -6,5 +6,3 @@ git-check-comment 'NO-COMMIT';
 git-check-comment 'NO-COMIT'; # make it work with typo
 git-check-comment 'BLOCKCOMMIT';
 git-check-comment 'BLOCK-COMMIT';
-git-check-comment 'BLOCK';
-git-check-comment 'STOP';
